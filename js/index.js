fb3e0286034b40b90f6c4f7009f2886879d61d26
Sven Notermans <nootje27@outlook.com>
2020-11-12 15:52:21 +0100

    added env file and getting data to the console.

--- js/index.js
+++ js/index.js
@@ -4,9 +4,15 @@ const URL = "http://localhost:3000/tweets";
  * Retrive Twitter Data from API
  */
 const getTwitterData = () => {
+    const url = "http://localhost:3000/tweets?q=coding&count=10";
+    fetch(url).then((response) => {
+        return response.json()
+    }).then((data) => {
+            console.log(data)
+        })
+    } 
 
-}
-
+    getTwitterData()
 /**
  * Save the next page data
  */
